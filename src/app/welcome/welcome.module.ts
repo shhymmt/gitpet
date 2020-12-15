43778cd33a7aff22906a3dcaae673f28a63efc32
shhymmt <mail@shhymmt.com>
2020-12-15 17:58:44 +0900

    チュートリアル6完了まで

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WelcomeComponent],
   imports: [
     CommonModule,
     WelcomeRoutingModule
